b67c5b7cd8d13ba757f6fc1a65d72827d10444bb
CristianCantoro <kikkocristian@gmail.com>
2013-07-19 11:06:49 +0200

    Adding commit log to dependencies

--- setup.py
+++ setup.py
@@ -20,9 +20,9 @@ required = [
     'dbpedia_querist==0.1',
 ]
 dependency_links = [
-    'git+https://github.com/SpazioDati/dewiki.git#egg=dewiki-0.1.0',
-    'git+https://github.com/SpazioDati/DataTXT-querist.git#egg=datatxt_querist-0.1',
-    'git+https://github.com/SpazioDati/DBpedia-querist.git#egg=dbpedia_querist-0.1',
+    'git+https://github.com/SpazioDati/dewiki.git@131f834089fa213152fd8edd4ebd206a7e53d581#egg=dewiki-0.1.0',
+    'git+https://github.com/SpazioDati/DataTXT-querist.git@b60823d03835d15da6063a3d512b685b2e024168#egg=datatxt_querist-0.1',
+    'git+https://github.com/SpazioDati/DBpedia-querist.git@9a5ae088702d20052fb8ad86180f36d119b1661e#egg=dbpedia_querist-0.1',
 ]
 
 setup(
