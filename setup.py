ed21729e364fc42ec27bfaf18f08966299b16e4b
CristianCantoro <kikkocristian@gmail.com>
2013-07-31 18:18:20 +0200

    Adding wikipedia_template_parser as a dependecy in setup.py

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ dependency_links = [
     'git+https://github.com/SpazioDati/dewiki.git@131f834089fa213152fd8edd4ebd206a7e53d581#egg=dewiki-0.1.0',
     'git+https://github.com/SpazioDati/DataTXT-querist.git@b60823d03835d15da6063a3d512b685b2e024168#egg=datatxt_querist-0.1',
     'git+https://github.com/SpazioDati/DBpedia-querist.git@d21e4f65f996305cb231ad4cac9bb341d65f05eb#egg=dbpedia_querist-0.1',
+    'git+https://github.com/SpazioDati/Wikipedia-Template-Parser.git@4c943f041fd010c9645f338090b76ebd41d07f6b#egg=wikipedia_template_parser',
 ]
 
 setup(
