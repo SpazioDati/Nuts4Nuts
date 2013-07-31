ca28cee95311879ed7332ec355ead52944503800
CristianCantoro <kikkocristian@gmail.com>
2013-07-31 18:12:06 +0200

    Updating commit no. for DBpedia-querist in setup.py

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ required = [
 dependency_links = [
     'git+https://github.com/SpazioDati/dewiki.git@131f834089fa213152fd8edd4ebd206a7e53d581#egg=dewiki-0.1.0',
     'git+https://github.com/SpazioDati/DataTXT-querist.git@b60823d03835d15da6063a3d512b685b2e024168#egg=datatxt_querist-0.1',
-    'git+https://github.com/SpazioDati/DBpedia-querist.git@9a5ae088702d20052fb8ad86180f36d119b1661e#egg=dbpedia_querist-0.1',
+    'git+https://github.com/SpazioDati/DBpedia-querist.git@d21e4f65f996305cb231ad4cac9bb341d65f05eb#egg=dbpedia_querist-0.1',
 ]
 
 setup(
